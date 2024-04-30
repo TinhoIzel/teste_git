6eb70f1351c9c1893f53c6ddc253e77abc91f418
TinhoIzel <tinhoizel@gmail.com>
2024-04-29 21:16:09 -0300

    opa

---
+++ teste.js
@@ -0,0 +1,9 @@
+// opa opa, só um teste
+
+// mudar a mensagem do commit
+// git commit --ammend -m "mensagem nova"
+
+// desfazer o commit
+// git reset --soft     -
+// git reset --mixed    -
+// git reset --hard     - volta pro último commit (reseta as mudanças)
