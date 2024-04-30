eaa1807292d603d9b470f4a953a03110e05b055f
TinhoIzel <tinhoizel@gmail.com>
2024-04-29 21:29:52 -0300

    opa3

--- teste.js
+++ teste.js
@@ -7,3 +7,6 @@
 // git reset --soft     -
 // git reset --mixed    -
 // git reset --hard     - volta pro último commit (reseta as mudanças)
+
+// trocar de branch:
+// git checkout -b "nomeDaBranch"
